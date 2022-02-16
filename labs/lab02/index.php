9baf20c55d98bb7a926d9243ecf59dbe35653938
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2022-02-16 19:18:31 +0000

    Add solution for q1

---
+++ labs/lab02/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Q1
+        $string = "The quick brown jumps.";
+        $brown = "brown"
+        echo substr_replace($string, $brown, 9, 0)
+    ?>
+</body>
+</html>
